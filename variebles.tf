83e496dca9614faa6aceb3a3b702b5b2bacf6a4e
efremandrei <145491486+efremandrei@users.noreply.github.com>
2024-06-25 11:34:05 +0300

    Update variebles.tf

--- variebles.tf
+++ variebles.tf
@@ -2,12 +2,3 @@ variable "region" {
     description = "value"
     default = "us-east-1"
 }
-
-variable "aws_access" {
-    description = "value"
-    default = "AKIAW4EHXWPWB5WQIR4A"
-}
-variable "aws_secret" {
-    description = "value"
-    default = "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"
-}
